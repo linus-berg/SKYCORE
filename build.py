18755bcb15c426e698f506e9556d1a1b06068b1f
linus-berg <linus@fenix.me.uk>
2024-03-17 16:32:20 +0100

    Initial commit.

---
+++ build.py
@@ -0,0 +1,53 @@
+import subprocess
+import json
+import glob
+import urllib.request
+import os
+
+if not os.path.exists("output"):
+    os.makedirs("output")
+ 
+def GetImage(id, image, tag):
+    print("Image:", image, tag)
+    subprocess.run([
+        "skopeo", 
+        "--override-arch",
+        "amd64",
+        "--override-os",
+        "linux",
+        "copy",
+        f"docker://{image}:{tag}",
+        f"docker-archive:output/skycore_image_{id}_{tag}.tar:{image}:{tag}"]) 
+
+def GetManifest(filename, url):
+    print("Manifest:", url)
+    urllib.request.urlretrieve(url, f"output/skycore_manifest_{filename}")
+
+def GetHelmChart(name, repo, chart, version):
+    print("Helm Chart:", repo, chart, version)
+    subprocess.run([
+        "helm", 
+        "pull",
+        chart,
+        "-d",
+        "output/",
+        "--repo",
+        repo
+        ]) 
+    filename = f"output/skycore_chart_{name}_{chart}_{version}.tgz"
+    os.rename(f"output/{chart}-{version}.tgz", filename)
+ 
+files = glob.glob("operators/*")
+for filepath in files:
+    # Opening JSON file
+    file = open(filepath)
+    operator = json.load(file)
+    for image in operator['images']:
+        GetImage(image['id'], image['image'], image['tag'])
+    for manifest in operator['manifests']:
+        GetManifest(manifest['filename'], manifest['url'])
+    for chart in operator['charts']:
+        GetHelmChart(operator['name'], chart['repo'], chart['chart'], chart['version'])
+    # Closing file
+    file.close()
+ 
---
+++ get-cluster-images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#
+
+kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec['initContainers', 'containers'][*].image}" | \
+tr -s '[[:space:]]' '\n' | \
+sort | \
+uniq -c
